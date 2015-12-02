b9342af95e400d91048e421cc4ee612c08735c84
Jeremy Beckman <jeremy.w.beckman@gmail.com>
2015-12-01 23:16:35 -0500

    Proj Setup

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 /*global require*/
+/*global __dirname*/
+/*global module*/
+/*global process*/
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
    res.render('index.ejs');
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
+module.exports = app;
